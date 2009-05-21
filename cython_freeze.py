0e846ab5c3f3b07674bf3cf27999186086006276
Mark Lodato <lodatom@gmail.com>
2009-05-20 21:33:40 -0400

    add documentation and example

--- cython_freeze.py
+++ cython_freeze.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+Create a C file for embedding one or more Cython source files.
+Requires Cython 0.11.2 (or perhaps newer).
+
+See README.rst for more details.
+"""
 
 import sys
 
